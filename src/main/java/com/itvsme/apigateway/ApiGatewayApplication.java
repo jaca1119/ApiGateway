1882012310dec25d130febb7d92cc1c46930e3bf
jaca1119 <laki1663@gmail.com>
2020-05-31 23:34:32 +0200

    Added load balanced route to other service by discovery server

--- src/main/java/com/itvsme/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/itvsme/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package com.itvsme.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication
 {
 
